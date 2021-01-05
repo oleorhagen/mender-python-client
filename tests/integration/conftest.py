df2e5c7326ed9c1ebb0af071617988779534aa41
Ole Petter <ole.orhagen@northern.tech>
2021-01-05 15:23:16 +0100

    Add the initial integration tests scaffolding
    Changelog: None
    Signed-off-by: Ole Petter <ole.orhagen@northern.tech>

---
+++ tests/integration/conftest.py
@@ -0,0 +1,5 @@
+
+from os import path
+import sys
+
+sys.path += [path.join(path.dirname(__file__), "mender_integration")]
---
+++ tests/integration/run.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+set -e
+
+MENDER_VERSION=${MENDER_VERSION:-master}
+
+# Generate docker-compose.testing.yml like integration's run.sh
+sed -e '/9000:9000/d' -e '/8080:8080/d' -e '/443:443/d' -e '/ports:/d' mender_integration/docker-compose.demo.yml > mender_integration/docker-compose.testing.yml
+sed -e 's/DOWNLOAD_SPEED/#DOWNLOAD_SPEED/' -i mender_integration/docker-compose.testing.yml
+sed -e 's/ALLOWED_HOSTS: .*/ALLOWED_HOSTS: ~./' -i mender_integration/docker-compose.testing.yml
+
+# Extract file system images from Docker images
+mkdir -p output
+docker run --rm --privileged --entrypoint /extract_fs -v $PWD/output:/output \
+       mendersoftware/mender-client-qemu:${MENDER_VERSION}
+mv output/* .
+rmdir output
+cp core-image-full-cmdline-qemux86-64.ext4 mender_integration/tests
+
+python -m pytest -v "$@"
