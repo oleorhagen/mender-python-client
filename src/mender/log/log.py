64faf293a566fe39ade55a44a4d617e5bb7f6b01
Ole Petter <ole.orhagen@northern.tech>
2021-01-18 12:58:06 +0100

    Marshal deployment logs upon deployment errors
    Changelog: None
    Signed-off-by: Ole Petter <ole.orhagen@northern.tech>

--- src/mender/log/log.py
+++ src/mender/log/log.py
@@ -11,12 +11,35 @@
 #    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
+import json
 import logging as log
 import logging.handlers
 import os
 import os.path
+from typing import List
 
 import mender.settings.settings as settings
+
+
+class JSONFormatter(logging.Formatter):
+    """JSON log formatter
+
+    Logs every log message to a json encoded line in the deployment.log file, in
+    the format:
+
+    {
+    "message": "foobar",
+    "timestamp": "UTC-...",
+    "level": "LEVEL"
+    }
+
+    """
+
+    def format(self, record) -> str:
+        level = record.levelname
+        message = record.message
+        timestamp = self.formatTime(record)
+        return json.dumps({"level": level, "timestamp": timestamp, "message": message,})
 
 
 class DeploymentLogHandler(logging.FileHandler):
@@ -24,7 +47,9 @@ class DeploymentLogHandler(logging.FileHandler):
         self.enabled = False
         self.log_dir = settings.PATHS.deployment_log
         filename = os.path.join(self.log_dir, "deployment.log")
+        self.log_file = filename
         super().__init__(filename=filename)
+        super().setFormatter(JSONFormatter())
 
     def handle(self, record):
         if self.enabled:
@@ -33,11 +58,26 @@ class DeploymentLogHandler(logging.FileHandler):
     def enable(self):
         self.enabled = True
         filename = os.path.join(self.log_dir, "deployment.log")
-        # Reset the log file
-        super().__init__(filename, mode="w")
+        # Reset the log
+        with open(filename, "w"):
+            pass
 
     def disable(self):
         self.enabled = False
+
+    def marshal(self) -> List[str]:
+        """Marshal the logs to the format required by the deployment endpoint"""
+        logs = []
+        try:
+            for line in open(self.log_file):
+                try:
+                    data = json.loads(line)
+                    logs.append(data)
+                except json.JSONDecodeError as e:
+                    log.error(f"Failed to marshal json, {e}")
+        except FileNotFoundError:
+            log.error("The log file was not found at: {self.log_file}")
+        return logs
 
 
 def add_sub_updater_log(log_file):
