91c6f2bce31132cc15a5858b7c83f0c331a47e6f
Ole Petter <ole.orhagen@northern.tech>
2021-01-18 17:49:14 +0100

    Make the deployment_logger optional
    Changelog: None
    Signed-off-by: Ole Petter <ole.orhagen@northern.tech>

--- src/mender/client/deployments.py
+++ src/mender/client/deployments.py
@@ -252,7 +252,7 @@ def report(
     deployment_id: str,
     server_certificate: str,
     JWT: str,
-    deployment_logger: Optional[menderlog.DeploymentLogHandler],
+    deployment_logger: Optional[menderlog.DeploymentLogHandler] = None,
 ) -> bool:
     """Report update :param status to the Mender server"""
     if not status:
