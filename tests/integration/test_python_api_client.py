ac879aab7e1979dfbf6c337a01dee708dc2f5c2b
Ole Petter <ole.orhagen@northern.tech>
2021-01-19 14:30:12 +0100

    MEN-4301: Test a failed update
    Changelog: None
    Signed-off-by: Ole Petter <ole.orhagen@northern.tech>

--- tests/integration/test_python_api_client.py
+++ tests/integration/test_python_api_client.py
@@ -19,7 +19,10 @@ import mender_integration.tests.conftest as cf
 cf.machine_name = "qemux86-64"
 
 from mender_integration.tests.common_setup import standard_setup_one_client_bootstrapped
-from mender_integration.tests.tests.common_update import update_image
+from mender_integration.tests.tests.common_update import (
+    update_image,
+    update_image_failed,
+)
 
 
 def test_update_successful(standard_setup_one_client_bootstrapped):
@@ -42,12 +45,44 @@ def test_update_successful(standard_setup_one_client_bootstrapped):
     )
 
 
-# def test_update_error():
-#     pass
+def test_update_error(standard_setup_one_client_bootstrapped):
+    """Test that the client behaves as expected upon failure.
+
+    This means that the deployment log from both the client itself, and the
+    sub-updater should be uploaded to the Mender server upon an error.
+
+    However, here we only check for the sub-updater log. The device deployment
+    log is tested in:
+
+    test_deployment_logs()
+
+    """
+
+    device = standard_setup_one_client_bootstrapped.device
+    update_image_failed(
+        standard_setup_one_client_bootstrapped.device,
+        standard_setup_one_client_bootstrapped.get_virtual_network_host_ip(),
+        install_image="broken_update.ext4",
+        expected_log_message="An update was seemingly in progress, and failed",
+    )
 
 
-# def test_deployment_logs():
-#     pass
+def test_deployment_logs():
+    """Test that the client handles a missing install script
+
+    The install script is supposed to be a symlink to the install script,
+    however, if it is missing, the client should report an error, and upload the
+    deployment logs.
+
+    """
+    device = standard_setup_one_client_bootstrapped.device
+    device.run("rm /usr/share/mender/install")
+    update_image_failed(
+        standard_setup_one_client_bootstrapped.device,
+        standard_setup_one_client_bootstrapped.get_virtual_network_host_ip(),
+        install_image="broken_update.ext4",
+        expected_log_message="Failed to run the install script",
+    )
 
 
 # def test_download_resume():
