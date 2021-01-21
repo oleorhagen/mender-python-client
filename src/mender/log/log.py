f31ccfd474ecff464e2b35137b3dea8457ec2db2
Ole Petter <ole.orhagen@northern.tech>
2021-01-21 11:16:57 +0100

    fix: Set the correct time format for the deployment logs
    Changelog: None
    Signed-off-by: Ole Petter <ole.orhagen@northern.tech>

--- src/mender/log/log.py
+++ src/mender/log/log.py
@@ -38,8 +38,8 @@ class JSONFormatter(logging.Formatter):
     def format(self, record) -> str:
         level = record.levelname
         message = record.message
-        timestamp = self.formatTime(record)
-        return json.dumps({"level": level, "timestamp": timestamp, "message": message,})
+        timestamp = self.formatTime(record, "%Y-%m-%dT%H:%M:%SZ")
+        return json.dumps({"level": level, "timestamp": timestamp, "message": message})
 
 
 class DeploymentLogHandler(logging.FileHandler):
